78aa4824a06ce13a788732390b844ebcdfef62a9
Resdan Logan <rezn5447@gmail.com>
2017-06-14 19:47:22 -0700

    Create 6-14_JS.js

--- 6-14_JS.js
+++ 6-14_JS.js
@@ -1,86 +1,98 @@
-// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+/*
 
-//For example, given the following list:
+                                              // PROBLEM #1 //
+You will be given an array of objects (associative arrays in PHP) representing data about developers
+who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object (associative array in PHP) which includes
+the count of each coding language represented at the meetup.
+
+For example, given the following input array:
 
 var list1 = [
-  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
-  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Europe', age: 30, language: 'JavaScript' },
-  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' },
-  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Europe', age: 30, language: 'JavaScript' }
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
 ];
+your function should return the following object (associative array in PHP):
 
-// Your task is to return the number of JavaScript developers coming from Europe.
-// Use .filter
+{ C: 2, JavaScript: 1, Ruby: 1 }
 
-// Should return 3
-
+*/
 
 
+
+
+/*
                                               // PROBLEM #2 //
-// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+You will be given an array of objects representing data about developers who have signed up to attend
+the next coding meetup that you are organising.
 
-// Hi < firstName here >, what do you like the most about < language here >?
-
-// For example, given the following input array:
+Given the following input array:
 
 var list1 = [
-  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
 ];
+  write a function that returns the average age of developers (rounded to the nearest integer). 
+  In the example above your function should return 50 (number).
 
-// Your function should return the following array:
-// [
-//    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
-//      greeting: 'Hi Sofia, what do you like the most about Java?'
-//    },
-//    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
-//      greeting: 'Hi Lukas, what do you like the most about Python?'
-//    },
-//    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
-//      greeting: 'Hi Madison, what do you like the most about Ruby?'
-//    } 
-// ];
+  Notes:
+  The input array will always be valid and formatted as in the example above.
+  Age is represented by a number which can be any positive integer. 
 
-// The trick is to return a person, adding the property as we go
-// you will need a block statement
-// Use .map
+*/
 
-
+/*
                                               // PROBLEM #3 //
-// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
-
-// Your task is to return one of the following strings:
-
-// < firstName here >, < country here > of the first Python developer who has signed up; or
-// There will be no Python developers if no Python developer has signed up.
-// For example, given the following input array:
+Given the following input array:
 
 var list1 = [
-  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
-  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
-  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
 ];
+write a function that adds the username property to each object in the input array:
 
-// Use .find and an If statement, or ternary =)
+[
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
+    username: 'emilyn1990' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', 
+    username: 'nore2000' }
+]
+  The value of the username property is composed by concatenating:
 
-// To embed javascript variables we use backticks ` and ${variable}
+  firstName in lower-case;
+  first letter of the lastName in lower-case; and
 
+  the birth year which for the purpose of this kata is calculated simply by subtracting age from the current year. 
+  Please make sure that your function delivers the correct birth year irrespective of when it will be executed, 
+  for example it should also work correctly for a meetup organised in 10-years-time. 
+  The example above assumes that the function is run in year 2020.
+
+  Notes:
+
+  The input array will always be valid and formatted as in the example above.
+  Age is represented by a number which can be any positive integer.
+  Lastname will always be one upper-cased letter followed by dot, e.g. 'N.'
+  Order of the objects in the array should be maintained but order of the properties in the individual objects does not matter. 
+
+*/
+
+/*
 
                                               // PROBLEM #4 //
-// You have an array of numbers.
-// Your task is to sort ascending odd numbers but even numbers must be on their places.
+  You have an array of numbers.
+  Your task is to sort ascending odd numbers but even numbers must be on their places.
 
-// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+  Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
 
-// Example
+  Example
 
-// sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+  sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 
-// Trick is to create an array of the odd numbers using filter, then map them back into the starting array:
+  Trick is to create an array of the odd numbers using filter, then map them back into the starting array:
 
-// const odds = arr.filter((n) => n % 2 != 0 )
-// then sort it odds.sort((a,b) => a - b) 
-
+  const odds = arr.filter((n) => n % 2 != 0 )
+  then sort it odds.sort((a,b) => a - b) 
+*/
