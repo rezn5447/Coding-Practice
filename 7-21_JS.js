291c227c942a24f3d60fee057aee19b471df2297
Resdan Logan <rezn5447@gmail.com>
2017-07-20 16:34:49 -0700

    Create 7-21_JS

---
+++ 7-21_JS.js
@@ -0,0 +1,35 @@
+//1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+function looper(n){
+
+}
+
+//2. Next, try looping just like above except using recursion
+function recLoop(n){
+
+
+}
+
+//3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+function expo(base,expo){
+
+
+}
+
+//4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function recurseExpo(base,expo){
+
+
+}
+
+//5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+function recurseMulti(arr,num){
+
+
+}
+
+
+//6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+function recurRev(arr){
+
+
+}
