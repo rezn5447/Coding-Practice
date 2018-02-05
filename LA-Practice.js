eb5bbc80dd079123d4175b70e3dbb29a8075ac0c
Resdan Logan <rezn5447@gmail.com>
2018-02-05 10:42:26 -0800

    Create LA-Practice.js

---
+++ LA-Practice.js
@@ -0,0 +1,84 @@
+
+// PROTIP YOU CAN USE CTRL + / TO COMMENT OUT ANY LINE OF CODE, TRY IT OUT!!!
+// CTRL + / TO UNCOMMENT AS WELL
+
+// Write a for loop that counts from 0 to 10 and AFTER ten console.logs('DONE!')
+// I'll write one that goes from 1 to five then DONE!
+
+
+for(var i = 1; i <= 6; i++){
+  
+  if(i <= 5){
+    console.log(i)
+  } else {
+    console.log('DONE!')
+  }
+  
+}
+
+
+// Write yours here:
+
+
+
+
+
+
+
+// Now write a for loop that counts BACKWARDS from 10 to 1 and AFTER ten console.logs('DONE!')
+// hint: use -- instead of ++
+
+
+
+// Write yours here:
+
+
+
+
+
+
+
+
+// Now write a while loop that does the same things as the other two for loops. (one that counts up, and one that counts down)
+// I'll write a while loop that starts at 5 and keeps going up to 50 by fives:
+
+// var i = 0
+// while(i < 50){
+//   i+= 5
+//   console.log(i)
+// }
+// console.log('WHILE OUT')
+
+
+
+
+
+
+// Write yours here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write yours here:
+
+
+
+
+
+
+
+
+
+
+
+
